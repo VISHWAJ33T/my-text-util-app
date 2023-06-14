ee19d8a3a1b13a9daea099975701e7d0f3d96793
Vishwajeet Yadav <79937035+VISHWAJ33T@users.noreply.github.com>
2023-06-14 21:45:01 +0530

    added switch in routes

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ function App() {
         <Alert alert={alert} />
         <div className="container my-3">
           <Routes>
+            <Switch>
             <Route
               path="/about"
               element={<Aboutus mode={mode} text={text} />}
@@ -71,6 +72,7 @@ function App() {
                 />
               }
             />
+            </Switch>
           </Routes>
         </div>
       </Router>
